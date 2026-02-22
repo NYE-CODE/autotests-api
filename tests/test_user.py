a97170a4149a2856c8e92f57f5e12864894a57b2
NYE-CODE <nikita@dspot.com.pl>
2026-02-22 15:54:52 +0300

    Написание API автотестов используя Pytest Практика написания API автотестов

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,7 @@ from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response
 
 
 def test_create_user():
